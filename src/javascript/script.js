049a91f8f5d9d26396a5546010503ccd27f0476f
Emanoel Pontes Timbo <emanoelpontes8@gmail.com>
2024-02-24 15:01:50 -0300

    Sidebar

---
+++ src/javascript/script.js
@@ -0,0 +1,14 @@
+const open = document.getElementById("open_btn");
+const sidebar = document.getElementById("sidebar");
+
+open.addEventListener("click", () => {
+  sidebar.classList.toggle("open-sidebar");
+});
+
+// Os dois códigos fazem a mesa coisa.
+
+// document.getElementById("open_btn").addEventListener("click", function () {
+//   document.getElementById("sidebar").classList.toggle("open-sidebar");
+// });
+
+//21:00
